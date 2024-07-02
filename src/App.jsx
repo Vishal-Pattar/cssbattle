f9a3d3ac346ff621a3c21eae77747aa62a2668da
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-02 21:02:44 +0530

    Updated src

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import AppContent from './components/AppContent/AppContent';
+import LandingPage from './components/LandingPage/LandingPage';
 
 const App = () => {
     return (
         <Router>
             <Routes>
+                <Route exact path="/" element={<LandingPage />} />
                 <Route path="/:id" element={<AppContent />} />
             </Routes>
         </Router>
