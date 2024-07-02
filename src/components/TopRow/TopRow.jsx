2960f391fb413f4b31a5a78b67f8eaab69bca1ab
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-02 19:21:57 +0530

    Added TopRow Component

---
+++ src/components/TopRow/TopRow.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TopRow.css';
+
+const TopRow = () => {
+    return (
+        <div className="top-row">
+            <div className='banner-text'>Creative-Minds presents CSS Battle</div>
+        </div>
+    );
+};
+
+export default TopRow;
