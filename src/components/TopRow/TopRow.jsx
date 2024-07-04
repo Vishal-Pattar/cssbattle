7c892495780694e130eea66faa0d5015a64a737f
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-04 16:54:33 +0530

    Updated TopRow

--- src/components/TopRow/TopRow.jsx
+++ src/components/TopRow/TopRow.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import './TopRow.css';
 
 const TopRow = () => {
+    const navigate = useNavigate();
+
+    // Function to handle click event
+    const navigateHome = () => {
+        navigate('/');
+    };
     return (
         <div className="top-row">
+            <button type='button' className='home-button' onClick={navigateHome}>Home</button>
             <div className='banner-text'>Creative-Minds presents CSS Battle</div>
         </div>
     );
