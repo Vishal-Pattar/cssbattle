348f229809ec3606b0255313bc5aac6187c1787d
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-04 21:05:56 +0530

    Updated Result Page

--- src/components/ResultPage/ResultPage.jsx
+++ src/components/ResultPage/ResultPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './ResultPage.css';
 import { IoCloseCircle } from "react-icons/io5";
 import useShowResult from '../../hooks/useShowResult';
+import GaugeChart from 'react-gauge-chart';
 
 const ResultPage = () => {
     const { showResult, toggleShowResult, score } = useShowResult();
@@ -18,6 +19,7 @@ const ResultPage = () => {
                 <div className='exit' onClick={handleExit}><IoCloseCircle size={'25px'} /></div>
                 <div className='text text1'>Congratulations</div>
                 <div className='text text2'>You scored</div>
+                <GaugeChart nrOfLevels={25} percent={score/100}  colors={["#FF0000", "#00FF00"]} hideText={true} needleColor="#FFFFFF" needleBaseColor="#FFFFFF"/>
                 <div className='text text3'>{score.toFixed(2) + " %"}</div>
             </div>
         </div>
