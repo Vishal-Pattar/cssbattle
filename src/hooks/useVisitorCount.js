e49142ccb2535f0e5c17fabd09505fe433224d69
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-07 03:49:15 +0530

    Added useVisitorCount Hook

---
+++ src/hooks/useVisitorCount.js
@@ -0,0 +1,23 @@
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const useVisitorCount = () => {
+    const [visitorCount, setVisitorCount] = useState();
+
+    const fetchVisitorCount = async () => {
+        try {
+            const response = await axios.get(`${process.env.REACT_APP_URL}/`);
+            setVisitorCount(response.data.cssbattle_visitor_count);
+        } catch (error) {
+            console.error("Error fetching visitor count:", error);
+        }
+    };
+
+    useEffect(() => {
+        fetchVisitorCount();
+    }, []);
+
+    return visitorCount;
+};
+
+export default useVisitorCount;
