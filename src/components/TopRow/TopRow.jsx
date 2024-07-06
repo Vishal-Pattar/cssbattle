76a82f70433b8e12f3e2e4c79885dc701e337a8c
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-07 03:19:33 +0530

    Updated TopRow

--- src/components/TopRow/TopRow.jsx
+++ src/components/TopRow/TopRow.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './TopRow.css';
+import useVisitorCount from '../../hooks/useVisitorCount';
 
 const TopRow = () => {
     const navigate = useNavigate();
-
-    // Function to handle click event
+    const visitorCount = useVisitorCount();
+    
     const navigateHome = () => {
         navigate('/');
     };
+
     return (
         <div className="top-row">
             <button type='button' className='home-button' onClick={navigateHome}>Home</button>
             <div className='banner-text'>CSS BATTLE</div>
-            <div className='visitor-count'>Visitor Count: 0</div>
+            <div className='visitor-count'>Visitor Count: {visitorCount}</div>
         </div>
     );
 };
