613464a4557ac8e5f737adb4ce09175e682284c9
Vishal-Pattar <vmpnwc15002@gmail.com>
2024-07-07 03:19:32 +0530

    Updated LandingPage

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LandingPage.css';
 import images from '../../helpers/importImages';
+import axios from 'axios';
+import useVisitorCount from '../../hooks/useVisitorCount';
 
 const imageKeys = Object.keys(images).filter(key => key.startsWith('expected_') && key.endsWith('.png'));
-
 const challenges = Array.from({ length: imageKeys.length }, (_, i) => ({
     id: i + 1,
     img: images[`expected_${i + 1}.png`],
@@ -19,6 +20,18 @@ const ChallengeBox = ({ id, img, onClick }) => (
 
 const LandingPage = () => {
     const navigate = useNavigate();
+    const visitorCount = useVisitorCount();
+
+    useEffect(() => {
+        const incrementVisitorCount = async () => {
+            try {
+                await axios.post(`${process.env.REACT_APP_URL}/increment-visitor`);
+            } catch (error) {
+                console.error("Error incrementing visitor count:", error);
+            }
+        };
+        incrementVisitorCount();
+    }, []);
 
     const handleClick = (id) => {
         navigate(`/${id}`);
@@ -27,7 +40,8 @@ const LandingPage = () => {
     return (
         <div className="landing-container">
             <div className='landing-header'>
-                <span>CSS BATTLE</span>
+                <div className='landing-title'>CSS BATTLE</div>
+                <div className='landing-count'>Visitor Count: {visitorCount}</div>
             </div>
             <div className='landing-box'>
                 {challenges.map((challenge) => (
